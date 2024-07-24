2546998d1960aca972bb7c284350e9066847ff27
Mateocheveri <cros746@gmail.com>
2024-07-24 16:03:27 -0500

    lista de tarea completada

--- 4-lista-de-tareas/app.js
+++ 4-lista-de-tareas/app.js
@@ -1,27 +1,56 @@
-const input = document.querySelector('.input')
+const input = document.querySelector('#input');
 const btncrear = document.querySelector('#btncrear');
 const btnborrar = document.querySelector('#btnborrar');
-const container = document.querySelector('.container');
-const lista  = document.querySelector('#lista')
+const main = document.querySelector('.container');
+const lista  = document.querySelector('#lista');
 let n = 0;
 
-function crear(){
-    const tarjeta = document.createElement('li');
 
-    tarjeta.innerText = '';
-    tarjeta.classList.add('lista li');
-    input.value = tarjeta;
-   
 
-  
+function crear (){
+    const lista = document.createElement("div");
+    lista.classList.add("lista");
+    main.appendChild(lista);
 
-    container.appendChild(tarjeta);
-    console.log(tarjeta);
+    const tachar = document.createElement("button");
+    tachar.innerText = "✔";
+    tachar.classList.add("tachar");
+    lista.appendChild(tachar);
+
+    const text = document.createElement("p")
+    text.innerText = input.value;
+    lista.appendChild(text); 
+    console.log(input.value)
+    
+    const eliminar = document.createElement("button");
+    eliminar.innerText = "X";
+    eliminar.classList.add("eliminar");
+    lista.appendChild(eliminar);
+
+
+    input.value = "";
 }
 
+function BorrarPantalla(){
+    main.innerHTML = "";
+    n =1;
+}
 
+function Borrar(e){
 
+    if(e.target.innerText === "X"){
+        main.removeChild(e.target.parentElement);
+    }
+}
 
+ function tachar(e){
+        
+    if(e.target.innerText === "✔"){
+        e.target.parentElement.style.textDecorationLine = "line-through"
+    }
+} 
 
-
-btncrear.addEventListener('click', crear);
+btncrear.addEventListener("click", crear);
+btnborrar.addEventListener("click", BorrarPantalla);
+main.addEventListener("click", Borrar);
+main.addEventListener("click", tachar);
