6726b8c33af73674b40ac4e98d0fcde3404eac85
Mateocheveri <cros746@gmail.com>
2024-08-14 16:00:32 -0500

    v3

--- 8-fetch-api/rickmorty.js
+++ 8-fetch-api/rickmorty.js
@@ -1,8 +1,71 @@
 const container = document.querySelector('.container');
+const buscador = document.querySelector('#buscador')
+const textoBusqueda = document.querySelector('#text')
+const listaStatus = document.querySelector('#listaStatus')
+let results;
+
+
+function pintar (Elemento){
+    switch(Elemento){
+        case 'Alive':
+            return 'green'
+        break
+        case 'Dead':
+            return 'red'
+        break
+        case 'unknown':
+            return 'gray'
+        break
+    }
+}
+
+function filtrar (e){
+    e.preventDefault()
+
+    container.innerHTML = ""
+
+    const filtro = results.filter( word => word.status == listaStatus.value);
+
+    console.log(filtro)
+
+    filtro.forEach( Element => {
+
+        const box =document.createElement('div')
+        box.classList.add("box")
+
+        box.innerHTML = `
+        <div class="card">
+            <img src="${Element.image}" class="card-img">
+
+            <div class="card-info">
+                <h2 class="card-title">${Element.name}</h2>
+                <h3 class="card-text"><i class='bx bxs-circle ${pintar(Element.status)}'></i>${Element.status}-${Element.species}</h3>
+                <h5 class="card-subtitle">Last known location:</h5>
+                <p class="card-text">${Element.location.name}</p>
+                <h5 class="card-subtitle">first seen in:</h5>
+                <p class="card-text"></p>                
+            </div>
+        </div>
+        `
+        container.appendChild(box)
+    }) 
+
+
+}
+
+buscador.addEventListener('submit' ,filtrar)
+
+
 let url = "https://rickandmortyapi.com/api/character"
 fetch(url)
 .then(Response => Response.json() )
 .then(data => {
+
+    results = data.results;
+
+
+    /* const result = data.results.filter(word => word.status == 'Alive');
+    console.log(result)
 
     data.results.forEach( Element => {
         const box =document.createElement('div')
@@ -15,7 +78,7 @@ fetch(url)
 
             <div class="card-info">
                 <h2 class="card-title">${Element.name}</h2>
-                <h3 class="card-text"><i class='bx bxs-circle'></i>${Element.status}-${Element.species}</h3>
+                <h3 class="card-text"><i class='bx bxs-circle ${pintar(Element.status)}'></i>${Element.status}-${Element.species}</h3>
                 <h5 class="card-subtitle">Last known location:</h5>
                 <p class="card-text">${Element.location.name}</p>
                 <h5 class="card-subtitle">first seen in:</h5>
@@ -24,5 +87,5 @@ fetch(url)
         </div>
         `
         container.appendChild(box)
-    }) 
+    })  */
 })
