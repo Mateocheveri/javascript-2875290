916e1ee0043ccb58c9290bc62e5c6f0aa0a42fc9
Mateocheveri <cros746@gmail.com>
2024-08-15 15:27:52 -0500

    fetch-api-rick

--- 8-fetch-api/rickmorty.js
+++ 8-fetch-api/rickmorty.js
@@ -1,60 +1,8 @@
 const container = document.querySelector('.container');
 const buscador = document.querySelector('#buscador')
-const textoBusqueda = document.querySelector('#text')
+const textoBusqueda = document.querySelector('#textoBusqueda')
 const listaStatus = document.querySelector('#listaStatus')
 let results;
-
-
-function pintar (Elemento){
-    switch(Elemento){
-        case 'Alive':
-            return 'green'
-        break
-        case 'Dead':
-            return 'red'
-        break
-        case 'unknown':
-            return 'gray'
-        break
-    }
-}
-
-function filtrar (e){
-    e.preventDefault()
-
-    container.innerHTML = ""
-
-    const filtro = results.filter( word => word.status == listaStatus.value);
-
-    console.log(filtro)
-
-    filtro.forEach( Element => {
-
-        const box =document.createElement('div')
-        box.classList.add("box")
-
-        box.innerHTML = `
-        <div class="card">
-            <img src="${Element.image}" class="card-img">
-
-            <div class="card-info">
-                <h2 class="card-title">${Element.name}</h2>
-                <h3 class="card-text"><i class='bx bxs-circle ${pintar(Element.status)}'></i>${Element.status}-${Element.species}</h3>
-                <h5 class="card-subtitle">Last known location:</h5>
-                <p class="card-text">${Element.location.name}</p>
-                <h5 class="card-subtitle">first seen in:</h5>
-                <p class="card-text"></p>                
-            </div>
-        </div>
-        `
-        container.appendChild(box)
-    }) 
-
-
-}
-
-buscador.addEventListener('submit' ,filtrar)
-
 
 let url = "https://rickandmortyapi.com/api/character"
 fetch(url)
@@ -63,6 +11,7 @@ fetch(url)
 
     results = data.results;
 
+    mostrarResults(data.results)
 
     /* const result = data.results.filter(word => word.status == 'Alive');
     console.log(result)
@@ -88,4 +37,106 @@ fetch(url)
         `
         container.appendChild(box)
     })  */
+ 
 })
+
+function pintar (Elemento){
+    switch(Elemento){
+        case 'Alive':
+            return 'green'
+        break
+        case 'Dead':
+            return 'red'
+        break
+        default:
+            return 'gray'
+    }
+}
+function mostrarResults(results){
+    results.forEach( Element => {
+
+        const box = document.createElement('div')
+        box.classList.add("box")
+
+        box.innerHTML = `
+        <div class="card">
+            <img src="${Element.image}" class="card-img">
+
+            <div class="card-info">
+                <h2 class="card-title">${Element.name}</h2>
+                <h3 class="card-text"><i class='bx bxs-circle ${pintar(Element.status)}'></i>${Element.status}-${Element.species}</h3>
+                <h5 class="card-subtitle">Last known location:</h5>
+                <p class="card-text">${Element.location.name}</p>
+                <h5 class="card-subtitle">first seen in:</h5>
+                <p class="card-text"></p>                
+            </div>
+        </div>
+        `
+        container.appendChild(box)
+    }) 
+
+}
+
+
+function filtrar (e){
+    e.preventDefault()
+
+    container.innerHTML = ""
+
+    if(listaStatus.value != "All"){
+
+        const filtro = results.filter( word => word.status == listaStatus.value).filter(word => word.name.toLowerCase().includes(textoBusqueda.value.toLowerCase()) );
+        //textoBusqueda.value.toLowerCase() == word.name.toLowerCase()
+
+        console.log(filtro)
+
+        mostrarResults(filtro)
+    }
+    else{
+        //""->false "content"->true
+        if(textoBusqueda.value){
+           const filtro = results.filter(word => word.name.toLowerCase().includes(textoBusqueda.value.toLowerCase()) );
+           mostrarResults(filtro)
+        }
+        else{
+            mostrarResults(results)
+        }
+
+        textoBusqueda.value = ""
+    }
+
+    /* const filtro = results.filter( word => word.status == listaStatus.value);
+
+    console.log(filtro)
+
+    mostrarResults(filtro) */
+
+
+    /*filtro.forEach( Element => {
+
+        const box =document.createElement('div')
+        box.classList.add("box")
+
+        box.innerHTML = `
+        <div class="card">
+            <img src="${Element.image}" class="card-img">
+
+            <div class="card-info">
+                <h2 class="card-title">${Element.name}</h2>
+                <h3 class="card-text"><i class='bx bxs-circle ${pintar(Element.status)}'></i>${Element.status}-${Element.species}</h3>
+                <h5 class="card-subtitle">Last known location:</h5>
+                <p class="card-text">${Element.location.name}</p>
+                <h5 class="card-subtitle">first seen in:</h5>
+                <p class="card-text"></p>                
+            </div>
+        </div>
+        `
+        container.appendChild(box)
+    }) */
+
+}
+
+buscador.addEventListener('submit' ,filtrar)
+
+
+
