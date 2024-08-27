fcc1353b145cae0cd8bb42e78e8531eab1759176
Mateocheveri <cros746@gmail.com>
2024-08-27 16:17:17 -0500

    trabajo final

---
+++ 10-chooj-app/js/bd.js
@@ -0,0 +1,22 @@
+let modulos = [
+    {
+        id: 1,
+        nombre: "nombre",
+        descripcion: "",
+        subtemas:[
+            {
+                titulo: "",
+                descripcion: "",
+            },
+            {
+                titulo: "",
+                descripcion: "",
+            },
+            {
+                titulo: "",
+                descripcion: "",
+            },
+        ]
+        
+    }
+]
