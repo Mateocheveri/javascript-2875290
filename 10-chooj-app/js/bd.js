6141e44fcda0e920e88d368aa83cd847b700220e
Mateocheveri <cros746@gmail.com>
2024-08-28 15:28:07 -0500

    proyecto final MOOC

--- 10-chooj-app/js/bd.js
+++ 10-chooj-app/js/bd.js
@@ -15,7 +15,67 @@ let modulos = [
             {
                 titulo: "",
                 descripcion: "",
+            }
+        ]
+        
+    },
+    {
+        id: 2,
+        nombre: "nombre",
+        descripcion: "",
+        subtemas:[
+            {
+                titulo: "",
+                descripcion: "",
             },
+            {
+                titulo: "",
+                descripcion: "",
+            },
+            {
+                titulo: "",
+                descripcion: "",
+            }
+        ]
+        
+    },
+    {
+        id: 3,
+        nombre: "nombre",
+        descripcion: "",
+        subtemas:[
+            {
+                titulo: "",
+                descripcion: "",
+            },
+            {
+                titulo: "",
+                descripcion: "",
+            },
+            {
+                titulo: "",
+                descripcion: "",
+            }
+        ]
+        
+    },
+    {
+        id: 4,
+        nombre: "nombre",
+        descripcion: "",
+        subtemas:[
+            {
+                titulo: "",
+                descripcion: "",
+            },
+            {
+                titulo: "",
+                descripcion: "",
+            },
+            {
+                titulo: "",
+                descripcion: "",
+            }
         ]
         
     }
